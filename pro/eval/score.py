f1d9d8c83b0bcd01bfc898a5ed9c61f78b1a1a87
IvanaXu <1440420407@qq.com>
2022-10-15 05:14:43 +0000

    #23 Change score.py for log N

--- pro/eval/score.py
+++ pro/eval/score.py
@@ -1,5 +1,5 @@
 import json
-import pandas as pd
+import numpy as np
 
 N = 5
 result = []
@@ -13,8 +13,14 @@ for n in range(1, N+1):
     result.append(
         json.loads(_r.replace("'", '"'))
     )
-result = [
+
+result = np.array([
     float(_r.get("score", "864000.0"))
     for _r in result
-]
-print(pd.DataFrame(result, columns=["score"]).describe())
+])
+
+print(f"\nEND result CNT: {len(result)} AVG: {np.mean(result):.4f}, MAX: {np.max(result):.4f}, MIN: {np.min(result):.4f}")
+result = [_r for _r in result if _r != 864000.0]
+if result:
+    print("DEL 864000.0")
+    print(f"END result CNT: {len(result)} AVG: {np.mean(result):.4f}, MAX: {np.max(result):.4f}, MIN: {np.min(result):.4f}")
