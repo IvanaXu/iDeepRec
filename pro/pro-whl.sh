0be09135f30e3766035d883d9364a50a925bb43b
IvanaXu <1440420407@qq.com>
2022-10-15 05:19:05 +0000

    #23 Change score.py for log N

--- pro/pro-whl.sh
+++ pro/pro-whl.sh
@@ -1,9 +1,10 @@
 
-# sh /pro/run-whl.sh|tee /pro/log/run-whl-epoch1.log
-# sh /pro/run-whl.sh|tee /pro/log/run-whl-epoch2.log
-# sh /pro/run-whl.sh|tee /pro/log/run-whl-epoch3.log
-# sh /pro/run-whl.sh|tee /pro/log/run-whl-epoch4.log
-# sh /pro/run-whl.sh|tee /pro/log/run-whl-epoch5.log
+#
+sh /pro/run-whl.sh|tee /pro/log/run-whl-epoch1.log
+sh /pro/run-whl.sh|tee /pro/log/run-whl-epoch2.log
+sh /pro/run-whl.sh|tee /pro/log/run-whl-epoch3.log
+sh /pro/run-whl.sh|tee /pro/log/run-whl-epoch4.log
+sh /pro/run-whl.sh|tee /pro/log/run-whl-epoch5.log
 
 # 
 python /pro/eval/score.py
