000eae98973ab10f43c77f37b4ae4e475347f025
IvanaXu <1440420407@qq.com>
2022-11-01 20:24:32 +0000

    #54 🌹 2.5.0

--- 04.run.sh
+++ 04.run.sh
@@ -1,7 +1,13 @@
 
 clear
-# rm -rf pkg/tensorflow_pkg/tensorflow-1.15.5+deeprec2206-cp36-cp36m-linux_x86_64.whl
-ls -l pkg/tensorflow_pkg/
+git pull
 
+cd pkg/tensorflow_pkg/
+rm -rf tensorflow-1.15.5+deeprec2206-cp36-cp36m-linux_x86_64.whl
+wget https://files.pythonhosted.org/packages/ef/17/01bdb5f1e47c1a5b39c58f138189df98e07feccf86935a834505f333ac6e/tensorflow-2.5.0-cp36-cp36m-manylinux2010_x86_64.whl
+mv tensorflow-2.5.0-cp36-cp36m-manylinux2010_x86_64.whl tensorflow-1.15.5+deeprec2206-cp36-cp36m-linux_x86_64.whl
+ls -l
+
+cd ../../
 # sh 00.cmd-configure.sh $1 && sh 01.cmd-submit.sh && sh 03.cmd-runWHL.sh $1
 sh 01.cmd-submit.sh && sh 03.cmd-runWHL.sh 1
--- pro/pro-whl.sh
+++ pro/pro-whl.sh
@@ -1,10 +1,10 @@
 
 #
 sh /pro/run-whl.sh|tee /pro/log/run-whl-epoch1-$1.log
-sh /pro/run-whl.sh|tee /pro/log/run-whl-epoch2-$1.log
-sh /pro/run-whl.sh|tee /pro/log/run-whl-epoch3-$1.log
-sh /pro/run-whl.sh|tee /pro/log/run-whl-epoch4-$1.log
-sh /pro/run-whl.sh|tee /pro/log/run-whl-epoch5-$1.log
+# sh /pro/run-whl.sh|tee /pro/log/run-whl-epoch2-$1.log
+# sh /pro/run-whl.sh|tee /pro/log/run-whl-epoch3-$1.log
+# sh /pro/run-whl.sh|tee /pro/log/run-whl-epoch4-$1.log
+# sh /pro/run-whl.sh|tee /pro/log/run-whl-epoch5-$1.log
 
 # 
-python /pro/eval/score.py $1
+# python /pro/eval/score.py $1
