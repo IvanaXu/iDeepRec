6b696c5ed233e6edf9246c3b8c06b2d9322dadb5
IvanaXu <1440420407@qq.com>
2022-11-01 20:27:26 +0000

    #54 🌹 2.4.0

--- 04.run.sh
+++ 04.run.sh
@@ -4,8 +4,8 @@ git pull
 
 cd pkg/tensorflow_pkg/
 rm -rf tensorflow-1.15.5+deeprec2206-cp36-cp36m-linux_x86_64.whl
-wget https://files.pythonhosted.org/packages/ef/17/01bdb5f1e47c1a5b39c58f138189df98e07feccf86935a834505f333ac6e/tensorflow-2.5.0-cp36-cp36m-manylinux2010_x86_64.whl
-mv tensorflow-2.5.0-cp36-cp36m-manylinux2010_x86_64.whl tensorflow-1.15.5+deeprec2206-cp36-cp36m-linux_x86_64.whl
+wget https://files.pythonhosted.org/packages/7a/ce/e76c4e3d2c245f4f20eff1bf9cbcc602109448142881e1f946ba2d7327bb/tensorflow-2.4.0-cp36-cp36m-manylinux2010_x86_64.whl
+mv tensorflow-2.4.0-cp36-cp36m-manylinux2010_x86_64.whl tensorflow-1.15.5+deeprec2206-cp36-cp36m-linux_x86_64.whl
 ls -l
 
 cd ../../
