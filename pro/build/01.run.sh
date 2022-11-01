3ac0d1e0b151ce1ed8d69562450e87ed46800488
IvanaXu <1440420407@qq.com>
2022-11-01 16:31:45 +0000

    #51 Build + absl by bazel

--- pro/build/01.run.sh
+++ pro/build/01.run.sh
@@ -6,8 +6,8 @@ apt-get install libprotobuf-dev protobuf-compiler -y
 
 echo
 ## abseil
-git clone https://github.com/abseil/abseil-cpp.git && cd abseil-cpp/
-bazel build //absl/...
+cd abseil
+bazel test --test_tag_filters=-benchmark @com_google_absl//...
 
 #
 echo && echo
